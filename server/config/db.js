99e78a8a6f3e8f37fc5877d1258bb959fc18ed88
Bhavna Pulliahgari <43325994+BhavnaP@users.noreply.github.com>
2019-02-25 13:02:54 -0500

    Add Functionality

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,8 @@
 module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://localhost/books308"
+
   //Remote MongoDB deployment -> let 
+  //MLab
   "URI" :"mongodb://thomas:123456@ds161059.mlab.com:61059/books308"
 };
--- server/routes/books.js
+++ server/routes/books.js
@@ -24,20 +24,37 @@ router.get('/', (req, res, next) => {
 });
 
 //  GET the Book Details page in order to add a new Book
+/* display add page */
 router.get('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  res.render("books/details", {
+    title: "Add New Book",
+  });
 
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let newBook = book({
+    "Title": req.body.title,
+    "Price": req.body.price,
+    "Author": req.body.author,
+    "Genre": req.body.genre
+  })
+
+  book.create(newBook, (err, book)=>{
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    }
+    else{
+      // refresh book list
+      res.redirect('/books');
+    }
+
+});
 
 });
 
